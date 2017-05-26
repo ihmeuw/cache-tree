3a806ce30b65e9a31c1ab0233990f8b4c21c307d
Aaron Keel <akeel@uw.edu>
2017-05-26 14:09:38 -0700

    create eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "semi": ["error", "always"],
+    "no-underscore-dangle": [0],
+    "no-param-reassign": [0]
+  }
+};
