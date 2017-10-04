7338014579b5af5954cc1b4abce7f75403373a39
Aaron Keel <akeel@uw.edu>
2017-10-04 10:09:51 -0700

    linked list class

---
+++ src/linked-list.ts
@@ -0,0 +1,54 @@
+import ListNode from './list-node';
+import Sentinel from './sentinel';
+
+export default class LinkedList<T> {
+  sentinel: Sentinel<T>;
+  length: number;
+  maxLength: number;
+
+  constructor(maxLength: number = 100000) {
+    this.sentinel = new Sentinel(null);
+
+    this.sentinel.next = this.sentinel;
+    this.sentinel.prev = this.sentinel;
+
+    this.length = 0;
+    this.maxLength = maxLength;
+  }
+
+  // insert node as sentinel next
+  insert(data: T) {
+    const newNode = new ListNode(Object.freeze(data));
+
+    newNode.prev = this.sentinel;
+    newNode.next = this.sentinel.next;
+
+    this.sentinel.next.prev = newNode;
+    this.sentinel.next = newNode;
+
+    this.length += 1;
+  }
+
+  // move node to sentinel next
+  refresh(node: ListNode<T>) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+
+    node.prev = this.sentinel;
+    node.next = this.sentinel.next;
+
+    this.sentinel.next.prev = node;
+    this.sentinel.next = node;
+  }
+
+  // remove node previous to sentinel
+  evict() {
+    const evicted = this.sentinel.prev;
+
+    this.sentinel.prev = evicted.prev;
+    this.sentinel.prev.next = this.sentinel;
+    evicted.clear();
+
+    this.length -= 1;
+  }
+}
