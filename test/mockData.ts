16bd07839cfd0b180ee9be8dabcd82f02e962a42
Aaron Keel <akeel@uw.edu>
2017-10-04 17:32:46 -0700

    create mock data for tests

---
+++ test/mockData.ts
@@ -0,0 +1,62 @@
+export default interface IData {
+  age: number;
+  estimate: number;
+  key: string;
+  location: number;
+  sex: number;
+  year: number;
+}
+
+export const d1 = {
+  age: 1,
+  estimate: 1,
+  key: 'brazil 1:1:1:1990:345',
+  location: 345,
+  sex: 1,
+  year: 1990,
+};
+
+export const d2 = {
+  age: 1,
+  estimate: 1,
+  key: 'brazil 1:1:1:2000:345',
+  location: 345,
+  sex: 1,
+  year: 2000,
+};
+
+export const d3 = {
+  age: 1,
+  estimate: 1,
+  key: 'brazil 1:1:1:2010:345',
+  location: 345,
+  sex: 1,
+  year: 2010,
+};
+
+export const d4 = {
+  age: 1,
+  estimate: 1,
+  key: 'brazil 1:1:1:1990:345',
+  location: 345,
+  sex: 1,
+  year: 1990,
+};
+
+export const d5 = {
+  age: 1,
+  estimate: 1,
+  key: 'brazil 1:1:1:2000:345',
+  location: 345,
+  sex: 1,
+  year: 2000,
+};
+
+export const d6 = {
+  age: 1,
+  estimate: 1,
+  key: 'brazil 1:1:1:2010:345',
+  location: 345,
+  sex: 1,
+  year: 2010,
+};
